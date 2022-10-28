cc91085a57574279f9a2e9c1ca2fb086a8c9c527
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-10-28 14:51:17 +0900

    共有

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,48 @@
+import 'dart:convert';
+import 'dart:developer' as dev;
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
+import 'package:mogura/secret.dart';
 
-void main() {
-  runApp(const MyApp());
+const dynamicLinksUri =
+    'https://firebasedynamiclinks.googleapis.com/v1/shortLinks';
+
+Future<String> _buildDynamicUrl(
+  Uri imageUrl,
+  int score,
+) async {
+  final response = await post(
+    Uri.parse(dynamicLinksUri)
+        .replace(queryParameters: {'key': firebaseAPIKey}),
+    body: jsonEncode({
+      "dynamicLinkInfo": {
+        "domainUriPrefix": "https://mogura.page.link",
+        "navigationInfo": {
+          "enableForcedRedirect": true,
+        },
+        "link": "https://mogura-tataki.web.app/",
+        "socialMetaTagInfo": {
+          "socialTitle": '光ったところを叩け',
+          "socialDescription": 'わたしのスコアは $score 回でした。あなたはどうする？',
+          "socialImageLink": imageUrl.toString(),
+        }
+      }
+    }),
+  );
+  dev.log(response.body);
+  return (jsonDecode(response.body) as Map<String, dynamic>)['shortLink'];
+}
+
+var url = '';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +50,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'モグラズ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -28,33 +67,52 @@ class MoguraPage extends StatefulWidget {
 }
 
 class _MoguraPageState extends State<MoguraPage> {
-  List<bool> moguras = List.generate(
-    9,
-    (index) => false,
-  );
+  static const playTime = 10;
+  static const sideLength = 5;
+  static const cellLength = 80.0;
 
   int count = 0;
 
-  int playTime = 10;
+  int timeLeft = playTime;
+
+  List<bool> moguras = List.generate(
+    sideLength * sideLength,
+    (index) => false,
+  );
 
   void mainLoop() async {
     count = 0;
-    playTime = 10;
-    while (playTime > 0) {
+    timeLeft = playTime;
+    while (timeLeft > 0) {
       moguras = List.generate(
-        9,
+        sideLength * sideLength,
         (index) => Random().nextBool(),
       );
-      playTime--;
+      timeLeft--;
       setState(() {});
       await Future.delayed(const Duration(seconds: 1));
     }
     moguras = List.generate(
-      9,
+      sideLength * sideLength,
       (index) => false,
     );
-    playTime = 10;
+    url = await _buildDynamicUrl(
+      Uri.parse(
+          'https://firebasestorage.googleapis.com/v0/b/mogura-tataki.appspot.com/o/sns.png?alt=media&token=bc6a6224-d07e-4072-8451-8788cf1541a8'),
+      count,
+    );
+    timeLeft = playTime;
     setState(() {});
+    // showDialog(
+    //   context: context,
+    //   builder: (context) {
+    //     return AlertDialog(
+    //       content: Column(
+    //         children: const [],
+    //       ),
+    //     );
+    //   },
+    // );
   }
 
   @override
@@ -67,25 +125,29 @@ class _MoguraPageState extends State<MoguraPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            for (var y = 0; y <= 2; y++)
+            const SizedBox(
+              height: 40,
+            ),
+            for (var y = 0; y < sideLength; y++)
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  for (var x = 0; x <= 2; x++)
+                  for (var x = 0; x < sideLength; x++)
                     InkWell(
                       onTap: () {
-                        if (moguras[x + (y * 3)]) {
-                          moguras[x + (y * 3)] = false;
+                        if (moguras[x + (y * sideLength)]) {
+                          moguras[x + (y * sideLength)] = false;
                           count++;
                           setState(() {});
                         }
                       },
                       child: Container(
-                        width: 80,
-                        height: 80,
+                        width: cellLength,
+                        height: cellLength,
                         alignment: Alignment.center,
-                        color: moguras[x + (y * 3)] ? Colors.red : Colors.grey,
-                        // child: Text('${x + (y * 3)}'),
+                        color: moguras[x + (y * sideLength)]
+                            ? Colors.red
+                            : Colors.grey,
                       ),
                     ),
                 ],
@@ -97,9 +159,29 @@ class _MoguraPageState extends State<MoguraPage> {
               ),
             ),
             ElevatedButton(
-              onPressed: playTime == 10 ? mainLoop : null,
+              onPressed: timeLeft == playTime ? mainLoop : null,
               child: const Text('スタート！'),
             ),
+            const SizedBox(height: 40),
+            if (url.isNotEmpty)
+              InkWell(
+                onTap: () async {
+                  await Clipboard.setData(ClipboardData(text: url));
+                  if (!mounted) {
+                    return;
+                  }
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('コピーしました')),
+                  );
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('結果をシェアする'),
+                    Icon(Icons.copy),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
